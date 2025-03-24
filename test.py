ee89174fa93ccb6831bd4a74f5c6bdfefbc1466e
anso-012 yeong <sohie.e03@gmail.com>
2025-03-24 03:24:27 +0000

    자료형 및 연산자를 활용

---
+++ test.py
@@ -0,0 +1,10 @@
+#number = int(input("출력할 구구단 숫자를 입력하세요: "))
+s_number = (input("출력할 구구단 숫자를 입력하세요: "))
+print(f"{s_number}단:")
+#print(number, "단:")
+#print(number + "단:")
+#print(f"{number}단, {number}단, {number}단")
+#number 값이 문자열이기 때문에 숫자로 변경해서 연산
+number = int(s_number)
+result = number * 2 
+print(result)
